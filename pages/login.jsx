5336a9aa3086d22f013524a2c45ba31c65df27c5
fariassilva <alessandrofarias405@gmail.com>
2023-05-18 08:42:18 -0300

    criado componente

---
+++ pages/login.jsx
@@ -0,0 +1,12 @@
+import ImageWithSpace from "../rsc/components/layout/ImageWithSpace"
+
+
+function LoginPage () {
+  return(
+    <ImageWithSpace>
+      teste 1234
+    </ImageWithSpace>
+  )
+}
+
+export default LoginPage
